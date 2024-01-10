e07931edd7196ea77fa3f96a878068bb15e3672f
efraguas <ericfraguas@gmail.com>
2024-01-10 01:07:48 +0100

    cambios

--- app/src/main/java/com/example/feedback_2b/MainActivity.kt
+++ app/src/main/java/com/example/feedback_2b/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.feedback_2b.adapter.AdapterBanderas
 import com.example.feedback_2b.data.DataSet
 import com.example.feedback_2b.databinding.ActivityMainBinding
 import com.example.feedback_2b.model.Pais
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     //para trabajar con la lista usamos un ArrayAdapter sera de tipo Pais que es lo que contiene mi lista
     private lateinit var adapterlista : ArrayAdapter<Pais>
+    private lateinit var adapadorPais : AdapterBanderas
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +24,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Los Ddatos de los paises estan en la clase DataSet
+        //adaptador para uso del recycler view le pasamos los dos parametros
+        //lista -> companion object del DataSet
+        //contexto -> hay que pasarle "this"
+        adapadorPais = AdapterBanderas(DataSet.getPaises(),this)
+        binding.recyclerPaises.adapter = adapadorPais
+        //ademas hay que añadirle un layoutManager
+        binding.recyclerPaises.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL,false)
+
+
+        //Los Datos de los paises estan en la clase DataSet
+
 
         //se le pasa el contexto, layout, y la lista
-        adapterlista = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, DataSet.getPaises())
+        //adapterlista = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, DataSet.getPaises())
 
-        //vinculo con la parte logica
-        binding.listaPaises.adapter = adapterlista
+        //vinculo con la parte logica usando listView
+        //binding.listaPaises.adapter = adapterlista
 
         // Cuando se haga pulsacion larga aparezca el nombre del pais en el TextView
-        binding.listaPaises.setOnItemLongClickListener { parent, view, position, id ->
-            binding.pais.setText("Pais: ${DataSet.getPaises()[position].nombre.toString()}")
-            true
-        }
+        //binding.listaPaises.setOnItemLongClickListener { parent, view, position, id ->
+        //    binding.pais.setText("Pais: ${DataSet.getPaises()[position].nombre.toString()}")
+        //    true
+        //}
 
         //click corto que pasaria los datos a la SecondActivity mediante un intent
-        binding.listaPaises.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent(this,SecondActivity::class.java)
-            intent.putExtra("pais",DataSet.getPaises()[position])
-            startActivity(intent)
-        }
+        //binding.listaPaises.setOnItemClickListener { parent, view, position, id ->
+        //    val intent = Intent(this,SecondActivity::class.java)
+        //    intent.putExtra("pais",DataSet.getPaises()[position])
+        //    startActivity(intent)
+        //}
 
 
     }
---
+++ app/src/main/java/com/example/feedback_2b/adapter/AdapterBanderas.kt
@@ -0,0 +1,76 @@
+package com.example.feedback_2b.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.feedback_2b.R
+import com.example.feedback_2b.model.Pais
+import com.bumptech.glide.Glide
+import com.example.feedback_2b.SecondActivity
+import com.example.feedback_2b.data.DataSet
+import com.google.android.material.snackbar.Snackbar
+
+
+class AdapterBanderas(var lista: ArrayList<Pais>, var contexto: Context): RecyclerView.Adapter<AdapterBanderas.Pais_holder>() {
+
+
+    //patron de la lista
+    class Pais_holder(vista:View):RecyclerView.ViewHolder(vista){
+        //extraer cada elemento del patron de la lista -> imagen, texto
+
+        var imagen_fila: ImageView
+        var texto_fila: TextView
+            init{
+                //inicializar las variables
+                imagen_fila = vista.findViewById(R.id.imagen_fila)
+                texto_fila = vista.findViewById(R.id.nombre_fila)
+            }
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Pais_holder {
+        //como se crea el patron
+
+        val vista: View = LayoutInflater.from(contexto).inflate(R.layout.item_banderas,parent,false)
+
+        return Pais_holder(vista)
+    }
+
+    override fun getItemCount(): Int {
+        //numero de elementos que tendra que adaptar
+        return lista.size
+    }
+
+    override fun onBindViewHolder(holder: Pais_holder, position: Int) {
+        //representa cada elememto de la lista en su posicion (utilizando el patron)
+        val item = lista[position]
+        holder.texto_fila.text = item.nombre
+        Glide.with(contexto).load(item.Imagen).into(holder.imagen_fila)
+
+        //desde aqui se gestionan las pulsaciones en un recyclerView
+
+
+        //click corto que pasaria los datos a la SecondActivity mediante un intent
+
+        holder.texto_fila.setOnClickListener {
+            val intent = Intent(contexto, SecondActivity::class.java)
+            intent.putExtra("pais", DataSet.getPaises()[position])
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            contexto.startActivity(intent)
+        }
+
+        holder.imagen_fila.setOnLongClickListener {
+            Snackbar.make(holder.imagen_fila,"Pais: ${DataSet.getPaises()[position].nombre.toString()}",Snackbar.LENGTH_SHORT).show()
+            true
+        }
+
+
+    }
+}
